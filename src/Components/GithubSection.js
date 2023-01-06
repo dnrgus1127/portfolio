5dbe74db304fbcc1036b0ca3b07a842445771617
dnrgus1127 <dnrgus1127@naver.com>
2023-01-07 01:04:43 +0900

    FEAT : Test MarkDown CSS

--- src/Components/GithubSection.js
+++ src/Components/GithubSection.js
@@ -6,13 +6,32 @@ const Container = styled.div`
   text-align: center;
   font-family: "Noto Sans KR", sans-serif;
   /* font-family: "Black Han Sans", sans-serif; */
-  height: 100vh;
-
-  h1 {
+  padding: 4% 0;
+  & > h1 {
     color: white;
     font-size: 6rem;
     font-family: inherit;
     font-weight: 800;
+    margin-bottom: 4%;
+  }
+`;
+const MarkdownCss = styled.div`
+  padding: 0 4%;
+  text-align: left;
+  li {
+    color: lightgrey;
+  }
+  ul {
+    margin: 20px;
+  }
+
+  & > p {
+    font-size: 1.4em;
+    color: white;
+  }
+  & > p:first-child {
+    font-size: 3em;
+    color: lightgreen;
   }
 `;
 export default function GithubSection() {
@@ -22,16 +41,21 @@ export default function GithubSection() {
     fetch("https://api.github.com/repos/dnrgus1127/ComponentProject/readme")
       .then((res) => res.json())
       .then((json) => {
-        fetch(json.download_url).then(res => res.text()).then(markdown => { setData(markdown); console.log(markdown) })
-
+        fetch(json.download_url)
+          .then((res) => res.text())
+          .then((markdown) => {
+            setData(markdown);
+            console.log(markdown);
+          });
       });
   }, []);
   return (
     <Container>
       <h1>깃허브</h1>
       {data ? <a href={data.html_url}>깃허브 바로가기</a> : null}
-      {data ? <ReactMarkdown >{data}</ReactMarkdown> : null}
-
+      <MarkdownCss>
+        {data ? <ReactMarkdown>{data}</ReactMarkdown> : null}
+      </MarkdownCss>
     </Container>
   );
 }
