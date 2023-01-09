1a22cde4c0cc73656af41798c47c4c3d5f65560f
dnrgus1127 <dnrgus1127@naver.com>
2023-01-10 01:05:17 +0900

    FEAT(기술스택) : 숙련도 게이지바 기능 추가중, 선택자에서 막힘

---
+++ src/Components/SkillGauge.js
@@ -0,0 +1,109 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+/// keyframe-------------------
+const ProficiencyFrame = keyframes`
+  0% {
+    width :0;
+  }
+  100% {
+    width: 100% ;
+  }
+`;
+//-----------------------keyframe
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  .per {
+    line-height: 4rem;
+    font-size: 1.6em;
+    margin-left: 1.5rem;
+  }
+`;
+
+const GaugeWrap = styled.ul`
+  margin: auto 0;
+  width: 80%;
+  height: 0.5rem;
+  list-style: none;
+  display: flex;
+  position: relative;
+  background-color: white;
+
+  li {
+    position: relative;
+    width: 20%;
+    height: 100%;
+    background-color: grey;
+    margin-right: 0.5rem;
+    box-shadow: 0px 0px 5px rgb(96, 96, 96, 0.7);
+  }
+
+  .painted::after {
+    position: absolute;
+    content: " ";
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 0%;
+    background-color: var(--light-color);
+    animation: ${ProficiencyFrame} 0.3s ease-in-out alternate forwards;
+  }
+
+  .painted {
+    /* background-color: var(--light-color); */
+  }
+
+  & .unPainted::after {
+    position: absolute;
+    content: " ";
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 0%;
+    background-color: grey;
+    animation: ${ProficiencyFrame} 0.3s ease-in-out alternate forwards;
+  }
+  li.unPainted:nth-of-type(4) {
+    background-color: green;
+    color: green;
+  }
+
+  & :nth-child(1)::after {
+    animation-delay: 0.3s;
+  }
+  & :nth-child(2)::after {
+    animation-delay: 0.6s;
+  }
+  & :nth-child(3)::after {
+    animation-delay: 0.9s;
+  }
+  & :nth-child(4)::after {
+    animation-delay: 1.2s;
+  }
+  & :nth-child(5)::after {
+    animation-delay: 1.5s;
+  }
+`;
+
+const createArray = (length) => [...Array(length)];
+
+// TODO : 숙련도 퍼센트 별로 색이 다르게, 처음 보일때나 hover 시에 숙련도가 차오르도록
+export default function SkillGauge({ per }) {
+  return (
+    <Container>
+      <GaugeWrap>
+        {createArray(5).map((n, i) => (
+          <li
+            key={i}
+            className={i < Math.trunc(per / 20) ? "painted" : "unPainted"}
+          ></li>
+        ))}
+      </GaugeWrap>
+      <p className='per'>{per}%</p>
+    </Container>
+  );
+}
--- src/Components/StackSection.js
+++ src/Components/StackSection.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import reactIcon from "../assets/img/pngwing.com.png";
+import SkillGauge from "./SkillGauge";
 
 const Container = styled.div`
   text-align: center;
   font-family: "Noto Sans KR", sans-serif;
-  padding: 3rem 6rem;
+  padding: var(--gap) var(--gap);
   h1 {
     /* font-size: 6rem; */
     font-family: inherit;
@@ -18,32 +19,50 @@ const Container = styled.div`
 const StackItem = styled.div`
   display: flex;
   justify-content: space-between;
+  .infoWrap {
+    width: 60%;
+  }
+  .iconWrap {
+    width: 40%;
+  }
 
-  & > div {
+  img {
     width: 50%;
   }
+  @media screen and (max-width: 500px) {
+    display: block;
 
-  .iconWrap {
+    .infoWrap {
+      width: 100%;
+    }
+    .iconWrap {
+      width: 100%;
+    }
   }
-  .infoWrap {
-    text-align: left;
+`;
+
+const StackTextStyle = styled.div`
+  text-align: left;
+  h2,
+  & > p {
+    margin-bottom: 2rem;
+  }
+  & > p {
+    font-size: 1.2em;
+    font-weight: 800;
   }
   h2 {
     position: relative;
     display: inline-block;
   }
-  .infoWrap h2::after {
+  & > h2::after {
     content: " ";
     position: absolute;
     left: 0;
     bottom: -4px;
     height: 3px;
     width: 100%;
-    background-color: var(--point-color);
-  }
-
-  img {
-    width: 50%;
+    background-color: var(--light-color);
   }
 `;
 
@@ -52,10 +71,24 @@ export default function StackSection() {
     <Container>
       <h1>기술 스택</h1>
       <StackItem className='stackSection'>
-        <div className='infoWrap'>
+        <StackTextStyle className='infoWrap'>
           <h2>리액트</h2>
-        </div>
-        <div>
+          <p>
+            사용자 인터페이스를 만들기 위해 사용되는{" "}
+            <span style={{ color: "#999900" }}>자바스크립트 </span>
+            라이브러리
+          </p>
+          <h3>숙련도</h3>
+          <div
+            className='gagueWrapper'
+            style={{ width: "100%", height: "4rem" }}
+          >
+            <SkillGauge per={60} />
+          </div>
+
+          <h3>정욱현's Skill</h3>
+        </StackTextStyle>
+        <div className='iconWrap'>
           <img src={reactIcon} alt='' />
         </div>
       </StackItem>
