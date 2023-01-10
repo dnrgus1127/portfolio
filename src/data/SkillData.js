aa71d13936c546b907306e91c70e72d16ac93f0d
dnrgus1127 <dnrgus1127@naver.com>
2023-01-10 17:36:01 +0900

    FEAT : 스택 영역 리스트 텍스트 데이터

---
+++ src/data/SkillData.js
@@ -0,0 +1,14 @@
+export const SkillData = {
+  React: {
+    skillName: "react",
+    mySkill: [
+      "리액트 훅에 대해서 이해하고 있습니다. ",
+      "styled-components를 사용하여 css를 적용합니다.",
+      "함수형 프로그래밍",
+      "컴포넌트의 재사용성에 대해서 고민합니다.",
+      "SPA에 대한 이해가 있습니다.",
+      "상태 관리 라이브러리 Redux, Recoil",
+      "React 상에서 TypeScript를 적용할 수 있습니다.",
+    ],
+  },
+};
